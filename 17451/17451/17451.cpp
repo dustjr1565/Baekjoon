ffa95c5f2e0592fdf87c3778253df7ffd2f8ccdc
chio yeon seo <gkwns9006@naver.com>
2020-09-10 19:23:39 +0900

    17451번 평행우주

--- 17451/17451/17451.cpp
+++ 17451/17451/17451.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	int* vArr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vArr[i];
+	}
+
+	long long result = vArr[n-1];
+	for (int i = n-2; i > -1; i--)
+	{
+		if (vArr[i] >= result)
+			result = vArr[i];
+		else
+			result = vArr[i] * ceil((double)result / (double)vArr[i]);
+	}
+
+	cout << result;
+	delete[] vArr;
+}
